9a05cdb29223a2f2638c68e4a7382ce1c2e84b52
James Kusmambang <kusmambang@gmail.com>
2021-03-28 20:35:03 +0700

    tipe data

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Belajar Golang
+
+#### Compile & Run
+```
+$ go build hello.go
+$ ./hello
+
+$ go run hello.go
+``` 
---
+++ go-basic/src/tipe_data.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// Number
+	fmt.Println(1)
+	fmt.Println(100)
+	fmt.Println(10.6)
+
+	// Boolean
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// String
+	fmt.Println("Ini String")
+	fmt.Println(len("ABC"))
+	fmt.Println(strings.ToUpper("upper"))
+	fmt.Println(strings.ToLower("LOWER"))
+}
