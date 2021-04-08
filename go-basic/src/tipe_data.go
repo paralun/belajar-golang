ddb28ec2a48b0b9ebb8b151e1ea05d9a2ea33d39
James Kusmambang <kusmambang@gmail.com>
2021-04-08 10:09:31 +0700

    konversi type data

--- go-basic/src/tipe_data.go
+++ go-basic/src/tipe_data.go
@@ -20,4 +20,17 @@ func main() {
 	fmt.Println(len("ABC"))
 	fmt.Println(strings.ToUpper("upper"))
 	fmt.Println(strings.ToLower("LOWER"))
+
+	// Konversi type data
+	var nilai32 int32 = 100000
+	var nilai64 int64 = int64(nilai32)
+
+	fmt.Println(nilai32)
+	fmt.Println(nilai64)
+
+	var nama = "Kusmambang"
+	var by byte = nama[1]
+	var kar string = string(by)
+
+	fmt.Println(kar)
 }
