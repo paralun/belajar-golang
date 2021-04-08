213ee89c0983d7c4c7720ac907abdf411c1f22e4
James Kusmambang <kusmambang@gmail.com>
2021-04-08 10:15:11 +0700

    type declarations

---
+++ go-basic/src/type_declarations.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// type declaration digunakan untuk membuat alias terhadap type data yg sudah ada
+	type Status string
+
+	var status1 Status = "Kawin"
+
+	fmt.Println(status1)
+	
+}
