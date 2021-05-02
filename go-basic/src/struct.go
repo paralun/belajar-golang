fe72ce0238741093ccd927a4b6668dbf2de7d431
James Kusmambang <kusmambang@gmail.com>
2021-05-02 16:47:51 +0700

    struct

---
+++ go-basic/src/struct.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Customer struct {
+	Name, Address string
+	Age int
+}
+
+func main() {
+	var cus1 Customer
+	cus1.Name = "Anissa"
+	cus1.Address = "Cianjur"
+	cus1.Age = 25
+
+	fmt.Println(cus1)
+	fmt.Println(cus1.Name)
+	fmt.Println(cus1.Address)
+	fmt.Println(cus1.Age)
+
+	cus2 := Customer{
+		Name:    "Budi",
+		Address: "Bandung",
+		Age:     30,
+	}
+	fmt.Println(cus2)
+
+	//harus sama urutannya
+	cus3 := Customer{"Bella", "Karawang", 22}
+	fmt.Println(cus3)
+}
