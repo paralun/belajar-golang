611d58347d512df50fe6fe32b2da4fb91c6d3c8d
James Kusmambang <kusmambang@gmail.com>
2021-05-27 10:21:59 +0700

    mutex

---
+++ go-goroutines/mutex_test.go
@@ -0,0 +1,62 @@
+package go_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+// Mutex untuk mengatasi masalah race condition
+func TestMutex(t *testing.T)  {
+	x := 0
+	var mutex sync.Mutex
+
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter ", x)
+}
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(balance int)  {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + balance
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+
+	return balance
+}
+
+func TestRWMutex(t *testing.T)  {
+	account := BankAccount{}
+
+	for i := 1; i <= 100; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance  ", account.GetBalance())
+}
